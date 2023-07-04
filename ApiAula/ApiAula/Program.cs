bf6384a659ac2c6096090f4a16d1792d40c67408
abrantesmar <abs.marcio@gmail.com>
2023-07-04 02:06:02 -0300

    adicionar o context no builder configuration do Program
    adicionar o context no builder configuration do Program

--- ApiAula/ApiAula/Program.cs
+++ ApiAula/ApiAula/Program.cs
@@ -1,4 +1,10 @@
+using ApiAula.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<ContextDB>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
